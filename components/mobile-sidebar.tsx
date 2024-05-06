3449feb978d1f546a951a52d8b24b768584a39b9
Bukan YOUTUBER <vradita29@gmail.com>
2024-05-06 21:49:42 +0700

    MobileSidebar Trigger

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger
+} from "@/components/ui/sheet";
+import { Sidebar } from "@/components/sidebar";
+
+export const MobileSidebar = () => {
+    return (
+        <div className="">
+            <Sheet>
+                <SheetTrigger className="md:hidden pr-4">
+                    <Menu />
+                </SheetTrigger>
+                <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+                    <Sidebar />
+                </SheetContent>
+            </Sheet>
+        </div>
+    )
+}
