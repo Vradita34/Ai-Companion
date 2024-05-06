be46c034ff425fc3a0bd79238f36c6316ffe723e
Bukan YOUTUBER <vradita29@gmail.com>
2024-05-06 21:32:06 +0700

    router sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,11 +2,13 @@
 
 import { Home, Plus, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
+import { useRouter } from 'next/navigation'
 
 import { cn } from "@/lib/utils";
 
 export const Sidebar = () => {
     const pathname = usePathname();
+    const router = useRouter();
 
     const routes = [
         {
@@ -29,12 +31,17 @@ export const Sidebar = () => {
         },
     ];
 
+    const onNavigate = (url: string, pro: boolean) => {
+        return router.push(url);
+    }
+
     return (
         <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
-            <div className="p-3 flex-1 justify-center">
+            <div className="p-3 flex flex-1 justify-center">
                 <div className="space-y-2">
                     {routes.map((route) => (
                         <div
+                            onClick={() => onNavigate(route.href, route.pro)}
                             key={route.href}
                             className={cn("text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
                                 pathname.startsWith(route.href) && "bg-primary/10 text-primary"
