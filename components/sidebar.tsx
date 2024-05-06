a938d42886520bb24a80b65ce39e0f82ac56447f
Bukan YOUTUBER <vradita29@gmail.com>
2024-05-06 21:07:43 +0700

    sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,9 +1,52 @@
 "use client";
 
+import { Home, Plus, Settings } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
 export const Sidebar = () => {
+    const pathname = usePathname();
+
+    const routes = [
+        {
+            icon: Home,
+            href: "/",
+            label: "Home",
+            pro: false
+        },
+        {
+            icon: Plus,
+            href: "/companion/new",
+            label: "Plus",
+            pro: true
+        },
+        {
+            icon: Settings,
+            href: "/settings",
+            label: "Settings",
+            pro: false
+        },
+    ];
+
     return (
         <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
-            Sidebar
+            <div className="p-3 flex-1 justify-center">
+                <div className="space-y-2">
+                    {routes.map((route) => (
+                        <div
+                            key={route.href}
+                            className={cn("text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
+                                pathname.startsWith(route.href) && "bg-primary/10 text-primary"
+                            )}>
+                            <div className="flex flex-col gap-y-2 items-center flex-1">
+                                <route.icon className="h-5 w-5" />
+                                <span>{route.label}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
-}
+};
