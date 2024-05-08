7ddcf0bf93233170356cf81ddd8b93a939520d3e
Bukan YOUTUBER <vradita29@gmail.com>
2024-05-08 22:57:23 +0700

    add button category and useRouter

--- app/(route)/(routes)/page.tsx
+++ app/(route)/(routes)/page.tsx
@@ -1,9 +1,15 @@
+import Categories from "@/components/categories";
 import { SearchInput } from "@/components/search-input";
+import prismadb from "@/lib/prismadb";
 
-export default function Home() {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany();
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 }
+
+export default RootPage;
